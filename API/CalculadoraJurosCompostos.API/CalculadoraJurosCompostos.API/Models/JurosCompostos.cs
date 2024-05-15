5705efc3e6f36bfdfff30f8b2c7c89c5df0f7a46
Aurel Lossou <aurelth@icloud.com>
2024-05-14 22:15:20 -0300

    Criação do Model JurosCompostos

---
+++ API/CalculadoraJurosCompostos.API/CalculadoraJurosCompostos.API/Models/JurosCompostos.cs
@@ -0,0 +1,11 @@
+﻿namespace CalculadoraJurosCompostos.API.Models
+{
+    public class JurosCompostos
+    {        
+        public decimal ValorInvestido { get; set; }
+        public double TaxaDeJuros { get; set; }
+        public int PeriodoInvestimento { get; set; }
+        public decimal ValorFuturo { get; set; }
+        public decimal GanhosEmJuros { get; set; }
+    }
+}
